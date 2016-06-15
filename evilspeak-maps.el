4be01bee6cb37c1728199fb563d9dbea41d2df0f
gloryofrobots <gloryofrobots@gmail.com>
2016-06-15 09:33:45 +0300

    first working movements

---
+++ evilspeak-maps.el
@@ -0,0 +1,4 @@
+(define-key evil-motion-state-map "j" 'evilspeak-next-visual-line)
+(define-key evil-motion-state-map "k" 'evilspeak-previous-visual-line)
+(define-key evil-motion-state-map "l" 'evilspeak-forward-char)
+(define-key evil-motion-state-map "h" 'evilspeak-backward-char)
